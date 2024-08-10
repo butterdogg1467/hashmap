8ded0c35d9175bb3502f356d9b5a5d6369690304
butterdogg1467 <momink2600@gmail.com>
2024-08-10 14:49:09 -0400

    Created set method and returned hash and set functions

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', function(){
         let buckets = new Array(size).fill(null)
         
         function hash(key) {
-            let hashCode = 0;
+            let hashCode = 0
             let modHashCode = 0
            
             const primeNumber = 31;
@@ -21,12 +21,33 @@ document.addEventListener('DOMContentLoaded', function(){
 
         
         function set(key, value){
-            let result = hash(key)
+            let index = hash(key)
+            if (buckets[index] === null) {
+                buckets[index] = []
+                let keyValuePair = {key: key, value: value}
+                buckets[index].push(keyValuePair)
+                console.log(buckets)
+            } else {
+                let keyValuePair = {key: key, value: value}
+                for (let i = 0; i < buckets[index].length; i++) {
+                    if (buckets[index][i].key === keyValuePair.key){
+                        buckets[index][i].value = keyValuePair
+                        console.log(buckets)
+                    } else {
+                        buckets[index][i].push(keyValuePair)
+                        console.log(buckets)
+                    }
+                }
+            }
 
         }
 
-    }
+        return {
+            hash,
+            set,
+        }
 
+    }
 
 
 
